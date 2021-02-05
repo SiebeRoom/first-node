95da1bc2848051348d39f21b51d5e2fb14240d7f
Siebe Room <Siebe.Room@student.vives.be>
2021-02-05 12:43:58 +0100

    second commit

--- Oefening1.js
+++ Oefening1.js
@@ -1,8 +1,12 @@
-function getGroup()
+function getGroup(datum)
 {
-    let datum = 20010519;
     let group = (datum % 7)+1;
-    console.log(group);
+    console.log('Uw geboortedatum is: ' + datum);
+    console.log('je bent toegewezen aan groep: ' + group);
 }
 
-getGroup;
+getGroup(20010519);
+getGroup(20200315);
+getGroup(20051111);
+getGroup(20061203);
+
