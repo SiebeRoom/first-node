1d8878074908b55927dab22478ed515f694f7006
Siebe Room <Siebe.Room@student.vives.be>
2021-02-05 12:55:28 +0100

    third commit

--- Oefening1.js
+++ Oefening1.js
@@ -5,9 +5,10 @@ const readline = require('readline').createInterface({
 
 function getGroup()
 {
-    readline.question('wat is uw leeftijd', datum => {
+    readline.question('wat is uw geboortedatum (JJJJMMDD): ', datum => {
         let group = (datum % 7)+1;
-        console.log('Uw geboortedatum is: ' + datum);
+        //geboortedatum is mooier geschreven
+        console.log('Uw geboortedatum is: ' + datum.substring(6,8) + '/' + datum.substring(4,6) + '/' + datum.substring(0,4));
         console.log('je bent toegewezen aan groep: ' + group);
         readline.close();
       });
